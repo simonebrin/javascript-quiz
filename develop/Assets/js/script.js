173b072bc820dd238dc8bdedf1e60ff5a46b867e
Simone Brin <simonebrin@gmail.com>
2022-07-04 20:54:17 -0400

    adding button function

--- develop/Assets/js/script.js
+++ develop/Assets/js/script.js
@@ -1,10 +1,26 @@
-Element.addEventListener(click, startQuiz)
+// Element.addEventListener(click, startQuiz)
 
-function clickHandler(event) {
-    console.log('Button Clicked');
- }
- 
+// // function clickHandler(event) {
+// //     console.log('Button Clicked');
+// //  }
 
-function startQuiz(){
+//  function startQuiz(){
+//   function clearBox(start){
+//     document.getElementById(start).innerHTML = "";
+//   }
+// }
 
-}
+// startQuiz()
+console.dir(window.document);
+
+var btn = window.document.querySelector("button");
+console.dir(btn);
+
+<button class="btn" id="start-quiz">Add Task</button>
+document.querySelector("#start-quiz");
+
+document.querySelector(".btn");
+document.querySelector("#start-quiz");
+
+var buttonEl = document.querySelector("#start-quiz");
+console.log(buttonEl);
