07c813cc1df58deccb41d58a74a04c64cb0e1c33
Simone Brin <simonebrin@gmail.com>
2022-07-07 17:14:49 -0400

    final css stuff ;ast commit

--- develop/Assets/js/script.js
+++ develop/Assets/js/script.js
@@ -1,29 +1,4 @@
-// Element.addEventListener(click, startQuiz)
 
-// // function clickHandler(event) {
-// //     console.log('Button Clicked');
-// //  }
-
-//  function startQuiz(){
-//   function clearBox(start){
-//     document.getElementById(start).innerHTML = "";
-//   }
-// }
-
-// startQuiz()
-// console.dir(window.document);
-
-// var btn = window.document.querySelector("button");
-// console.dir(btn);
-
-// {/* <button class="btn" id="start-quiz">Add Task</button>
-// document.querySelector("#start-quiz"); */}
-
-// document.querySelector(".btn");
-// document.querySelector("#start-quiz");
-
-// var buttonEl = document.querySelector("#start-quiz");
-// console.log(buttonEl);
 
 var myH1 = document.querySelector('#pageTitle')
 var myButton = document.getElementById('startQuiz')
@@ -149,9 +124,6 @@ function showQuiz(i) {
         if (element.matches('.btn')){
             if (element.textContent === myQuestions[currentQuestionIndex].correctAnswer) {
                 score++
-                // var score = document.getElementsByClassName("score");
-                // scores.innerText = score;
-                classToApply = 'correct'
                 console.log(score)
             }else{
                 console.log('wrong answer')
@@ -163,7 +135,6 @@ function showQuiz(i) {
                 endQuiz();
                 }
             }
-            // showQuiz(currentQuestionIndex)
         }
     )
 
@@ -173,85 +144,9 @@ function showQuiz(i) {
         scores.textContent = "You scored " + score + "/5, congrats!"
         questionElement.classList.add('hide')
         answerButtonsElement.classList.add('hide')
+        
     }
 
     function showScore() {
         score.classList.remove('hide')
     }
-
-// If you are at the last question end the quiz, showing a game over screen taking them to highscore page
-// if it was wrong answer, decrease the time
-
-// answerButtonsElement.addEventListener('click', function(event) {
-//     var element = event.target
-//     if (element.matches('.btn')){
-//         if (element.textContent === myQuestions[currentQuestionIndex].correctAnswer) {
-//             console.log('great job')
-//         }else{
-//             console.log('wrong answer')
-//         }
-//         currentQuestionIndex++
-//         showQuiz(currentQuestionIndex)
-//     }
-// })
-
-
-//     var quizContainer = document.getElementById('quiz');
-//     var resultsContainer = document.getElementById('results');
-//     var submitButton = document.getElementById('submit');
-//     var startQuizButton = document.getElementById('startQuiz')
-//     var myQuestions = [ 
-//         {
-//             question: "something about javascript?",
-//             answers: {
-//                 a: "answer1",
-//                 b: "answer2",
-//                 c: "answer3"
-//             },
-//             correctAnswer: "b"
-//         },
-//         {
-//             question: "something else about javascript?",
-//             answers: {
-//                 a: "answer1",
-//                 b: "answer2",
-//                 c: "answer3"
-//             },
-//             correctAnswer: "c"
-//         },
-//         {
-//             question: "something else else about javascript?",
-//             answers: {
-//                 a: "answer1",
-//                 b: "answer2",
-//                 c: "answer3"
-//             },
-//             correctAnswer: "a"
-//         }
-//     ];
-
-
-
-// // Pagination
-// var previousButton = document.getElementById("previous");
-// var nextButton = document.getElementById("next");
-// var slides = document.querySelectorAll(".slide");
-// let currentSlide = 0;
-
-
-
-// // Event listeners
-// startQuizButton.addEventListener('click', startQuiz);
-// // previousButton.addEventListener("click", showPreviousSlide);
-// // nextButton.addEventListener("click", showNextSlide);
-
-
-
-
-
-
-   
-
-
-
-
